2b79336316da02ef400c0d4d42196e348a5aebe4
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-10-26 02:53:46 +0900

    add another button color setting.

--- src/components/parts/button/PartsSimpleButton.tsx
+++ src/components/parts/button/PartsSimpleButton.tsx
@@ -15,7 +15,7 @@ export const PartsSimpleButton: React.VFC<Props> = ({
   return (
     <button
       // className={`parts-simple-button util-color__bg--${color} util-color__text--${textColor}`}
-      className={`parts-simple-button util-color__text--${textColor}`}
+      className={`parts-simple-button parts-simple-button__color--${color} util-color__text--${textColor}`}
     >
       {text}
     </button>
