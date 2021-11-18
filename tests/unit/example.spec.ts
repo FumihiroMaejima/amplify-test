8ccab9f46866d5e3661530448d89ab90d9f95ac4
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-11-19 02:14:04 +0900

    modify unit test setting.

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,18 +1,15 @@
-import React from 'react'
-import { OptionalEventProperties, createRenderer } from 'react-dom/test-utils'
-import { render } from '@testing-library/react'
-import { GlobalFooter } from '@/components/_global/GlobalFooter'
+// import React from 'react'
+// import { OptionalEventProperties, createRenderer } from 'react-dom/test-utils'
+// import { render } from '@testing-library/react'
+import { GlobalHeader } from '@/components/_global/GlobalHeader'
 
-import 'jest'
-// import { shallowMount } from '@types/jest'
-
-describe('GlobalFooter.vue', () => {
+describe('GlobalHeader.tsx', () => {
   it('test message', () => {
     /* const renderResult = render(<GlobalFooter />)
     expect(renderResult.innerHTML).toBe('hoge') */
     const msg = 'new message'
-    const wrapper = GlobalFooter
-    createRenderer()
-    expect(wrapper.name).match('GlobalFooter')
+    const wrapper = GlobalHeader
+    // createRenderer()
+    expect(wrapper.name).toBe('GlobalHeader')
   })
 })
