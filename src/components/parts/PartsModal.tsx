f052251c3d8d3653bd75681ccaa2b01c4229665b
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-11-30 00:46:57 +0900

    modify modal design.

--- src/components/parts/PartsModal.tsx
+++ src/components/parts/PartsModal.tsx
@@ -9,14 +9,22 @@ export const PartsModal: React.VFC = () => {
     <div className="parts-modal">
       <div className="parts-modal__dialog">
         <div className="parts-modal__content">
-          <h1>modal header</h1>
-          <p>test 1234 modal content!</p>
+          <div className="parts-modal__content-head">
+            <h1>modal header</h1>
+          </div>
           <button className="parts-modal__close">
             <svg className="" viewBox="0 0 24 24">
               <path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z" />
               <path d="M0 0h24v24h-24z" fill="none" />
             </svg>
           </button>
+          <div className="parts-modal__content-body">
+            <p>test 1234 567890 abcdefg modal content!</p>
+            <p>test 1234 567890 abcdefg modal content!</p>
+            <p>test 1234 567890 abcdefg modal content!</p>
+            <p>test 1234 567890 abcdefg modal content!</p>
+            <p>test 1234 567890 abcdefg modal content!</p>
+          </div>
         </div>
       </div>
     </div>
