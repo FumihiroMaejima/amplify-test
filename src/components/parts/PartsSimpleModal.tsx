294db535a87a765e42f9396c1490735783c696e7
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-12-01 01:31:28 +0900

    add modal close handler.

--- src/components/parts/PartsSimpleModal.tsx
+++ src/components/parts/PartsSimpleModal.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 type Props = {
   isOpen: boolean
+  closeModalHandler: () => void
 }
 
-export const PartsSimpleModal: React.VFC<Props> = ({ isOpen = false }) => {
+export const PartsSimpleModal: React.VFC<Props> = ({
+  isOpen = false,
+  closeModalHandler = () => {
+    console.log('default')
+  },
+}) => {
   return (
     <div
       className={`parts-simple-modal ${
@@ -20,7 +26,10 @@ export const PartsSimpleModal: React.VFC<Props> = ({ isOpen = false }) => {
           <div className="parts-simple-modal__content-head">
             <h1>modal header</h1>
           </div>
-          <button className="parts-simple-modal__close">
+          <button
+            className="parts-simple-modal__close"
+            onClick={closeModalHandler}
+          >
             <svg className="" viewBox="0 0 24 24">
               <path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z" />
               <path d="M0 0h24v24h-24z" fill="none" />
--- src/pages/Test1.tsx
+++ src/pages/Test1.tsx
@@ -9,31 +9,35 @@ import { PartsSimpleModal } from '@/components/parts/PartsSimpleModal'
 export const Test1: React.VFC = () => {
   const baseImageHost = 'https://picsum.photos/'
 
-  // let testOpenValue = false
-  /* const testClickHandler: MouseEventHandler<HTMLButtonElement> = (
-    event: MouseEvent<HTMLButtonElement>
-  ): void => {
-    // console.log('click event: ', JSON.stringify(event, null, 2))
-    console.log('click event befor: ', JSON.stringify(testOpenValue, null, 2))
-    testOpenValue = true
-    console.log(
-      'click event after: ',
-      JSON.stringify(testOpenValue, null, 2)
-    )
-  } */
+  const [isOpenModal, updateOpenModalFlad] = useState(false)
 
-  const [testval, setTest] = useState(false)
-  const testClickHandler: MouseEventHandler<HTMLButtonElement> = (
+  /**
+   * update modal open flag.
+   * @return {void}
+   */
+  const changeOpenModalValueHandler = (): void => {
+    updateOpenModalFlad(!isOpenModal)
+  }
+
+  /**
+   * modal open button click event handler
+   * @param {React.MouseEvent<HTMLButtonElement>} event
+   * @return {MouseEventHandler<HTMLButtonElement>}
+   */
+  const openModalHandler: MouseEventHandler<HTMLButtonElement> = (
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     event: React.MouseEvent<HTMLButtonElement>
   ): void => {
-    setTest(!testval)
+    changeOpenModalValueHandler()
   }
 
   return (
     <div className="page-container page-container__mx-auto">
       {/* <PartsCircleLoading /> */}
-      <PartsSimpleModal isOpen={testval} />
+      <PartsSimpleModal
+        isOpen={isOpenModal}
+        closeModalHandler={changeOpenModalValueHandler}
+      />
       <PartsSimpleHeading text="サンプル 画像 ページ" color="dark-grey" />
       <div className="m-x2">
         <PartsLabelHeading text="サブヘッダー1" color="blue" />
@@ -45,7 +49,7 @@ export const Test1: React.VFC = () => {
 
         <div className="util-text__contents-area util-border-full-solid-2p__color--dark-grey util-border-radius__round--5p util-color__text--dark-grey">
           <PartsSimpleButton text="blue" color="blue" />
-          <button onClick={testClickHandler}>test button</button>
+          <button onClick={openModalHandler}>test button</button>
         </div>
 
         <PartsLabelHeading text="サンプル画像 200px * 200px" color="green" />
