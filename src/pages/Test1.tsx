78ce5c2a067b28fc6c4675895c3dbebfa7efa96c
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-12-04 23:51:47 +0900

    modify image id.

--- src/pages/Test1.tsx
+++ src/pages/Test1.tsx
@@ -59,9 +59,12 @@ export const Test1: React.VFC = () => {
         <PartsLabelHeading text="サンプル画像 200px * 200px" color="green" />
         <div className="util-text__contents-area util-border-full-solid-2p__color--dark-grey util-border-radius__round--5p util-color__text--dark-grey">
           {/* <img src={`${baseImageHost}id/290/200/200`} alt="sample image"></img> */}
-          <img src={`${baseImageHost}id/292/200/200`} alt="sample image"></img>
+          {/* <img src={`${baseImageHost}id/292/200/200`} alt="sample image"></img> */}
           {/* <img src={`${baseImageHost}id/295/200/200`} alt="sample image"></img> */}
-          <img src={`${baseImageHost}id/299/200/200`} alt="sample image"></img>
+          {/* <img src={`${baseImageHost}id/299/200/200`} alt="sample image"></img>
+          <img src={`${baseImageHost}id/300/200/200`} alt="sample image"></img> */}
+          <img src={`${baseImageHost}id/301/200/200`} alt="sample image"></img>
+          <img src={`${baseImageHost}id/306/200/200`} alt="sample image"></img>
         </div>
       </div>
 
