b1a5c548d8b641a93c0d15f5cde53f2deb7aad2e
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-12-18 01:40:30 +0900

    add i18n setting.(internationalization)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
-import Amplify from 'aws-amplify'
+import Amplify, { I18n } from 'aws-amplify'
 import awsConfig from '@/aws-exports'
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+
+// 日本語化対応
+import { translations } from '@aws-amplify/ui'
+I18n.putVocabularies(translations)
+I18n.setLanguage('ja')
 
 // import logo from '@/assets/img/logo.svg'
 import { AppRouter } from '@/AppRouter'
@@ -17,7 +22,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Authenticator>
+    <Authenticator variation="modal">
       {({ signOut, user }) => (
         <div className="app">
           <GlobalHeader />
